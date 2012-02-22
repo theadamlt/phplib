ee030385b99531f4057ab4fbe87abe8814cf5a96
Adam Lilienfeldt <lilienfeldt.adam@gmail.com>
2012-02-22 09:10:09 +0100

    Added fucntions

--- lib.php
+++ lib.php
@@ -14,16 +14,16 @@ if (defined('ENVIRONMENT'))
 	switch (ENVIRONMENT)
 	{
 		case 'development':
-			error_reporting(E_ALL);
+		error_reporting(E_ALL);
 		break;
-	
+		
 		case 'testing':
 		case 'production':
-			error_reporting(0);
+		error_reporting(0);
 		break;
 
 		default:
-			exit('The application environment is not set correctly.');
+		exit('The application environment is not set correctly.');
 	}
 }
 
@@ -45,27 +45,31 @@ function mysql_con($host, $user, $password, $dbname)	{
 function localhost_con($dbname) {
 	global $con;
 	$con = mysql_connect("localhost", "root", "");
-		if (!$con)	{
+	if (!$con)	{
 		die ("Could not connect to MySQL database ".mysql_error());
 	}
 	global $con_db;
 	$con_db = mysql_select_db($dbname,$con);
-		if (!$con_db) {
+	if (!$con_db) {
 		die ("Could not select database ".$dbname." ".mysql_error());
 	}
 }
 
-function getRemoteaddr()	{
-	$ip = $_SERVER[REMOTE_ADDR];
-	return $ip;
-}
-function getUserAgent()	{
-	$useragent = $_SERVER ['HTTP_USER_AGENT'];
-   	 return $useragent;
-}
-
 function mysql_fix_string($string)	{
-	if (get_magic_quotes_gpc()) $string stripslashes($string);
+	if (get_magic_quotes_gpc()) $string = stripslashes($string);
 	return mysql_real_escape_string($string);
 }
+
+function mysql_enteries_fix_string($string) {
+	return htmlentities(mysql_fix_string($string));
+}
+
+function oddOrEven($num)
+{
+if($odd = $num%2 ) return(0); //Odd number
+else return(1); //Even number
+}
+
+$salt1 = "/*5t12?";
+$salt2 = "45¤¤5j";
 ?>
