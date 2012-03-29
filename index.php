8cd3bb5eef65253b037bcdf0005b689bccb8c323
Adam Lilienfeldt <lilienfeldt.adam@gmail.com>
2012-03-29 08:20:06 +0200

    commit

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 require('lib.php');
-echo detect_browser();
+$string = "lorem ipsum dolor sit amet, Consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+echo format_mail($string);
 
 ?>
